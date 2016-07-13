4659940a227765af3e822fc902c21c711be8dd22
TheDJ42 <TheDJ42@github.com>
2016-07-13 10:15:20 -0600

    Finishes project two

--- projects/hello_part2/hello.js
+++ projects/hello_part2/hello.js
@@ -1,8 +1,14 @@
-document.getElementById("button").onclick=function() {addLi()};
-document.getElementById("intel").onclick=function() {addLi()};
-document.getElementById("eol").onclick=function() {addLi()};
+document.getElementById("button").onclick=function() {addLi(helloStr)};
+document.getElementById("intel").onclick=function() {addLi(linusStr)};
+document.getElementById("eol").onclick=function() {addLi(flynn)};
 
-function addLi(){
+
+//vars of the text on the page
+
+var helloStr = "Hello World";
+var linusStr = "Intelligence is the ability to avoid doing work, yet getting the work done.";
+var flynn = "End of line, man";
+function addLi(text){
 	
 	var u = document.getElementById("mainUL");
 	var child = document.getElementsByTagName("li");
@@ -12,7 +18,7 @@ function addLi(){
 	}
 	
 	var l = document.createElement("LI");
-	var x = document.createTextNode("text");
+	var x = document.createTextNode(text);
 	l.appendChild(x);
 	u.appendChild(l);
 	
